0cb84fea44dca2473bc3137f1735cf5b9e33f261
Di <zzloba@gmail.com>
2026-03-30 16:20:41 +0300

    Add vercel.json proxy for API

--- src/api/users.js
+++ src/api/users.js
@@ -1,18 +1,15 @@
-const API_URL = '/api';
-
 export const fetchUsers = async () => {
   try {
-    const response = await fetch(`${API_URL}/users`);
+    const response = await fetch('/api/users');
     
     if (!response.ok) {
-      throw new Error(`Error fetchUsers: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
     
     const data = await response.json();
-    console.log('Users:', data);
     return data.slice(0, 6);
   } catch (error) {
-    console.error('Error fetchUsers:', error);
+    console.error('Error fetching users:', error);
     throw error;
   }
 };
